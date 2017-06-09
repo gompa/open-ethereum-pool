c332ffda752aead2a88c183fbdab8fd8331ac96b
Gompa <gompa@h-bomb.nl>
2017-06-09 07:09:40 +0200

    Create env.sh

--- build/env.sh
+++ build/env.sh
@@ -10,7 +10,7 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/GRinvest"
+ethdir="$workspace/src/github.com/gompa"
 if [ ! -L "$ethdir/open-ethereum-pool" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
